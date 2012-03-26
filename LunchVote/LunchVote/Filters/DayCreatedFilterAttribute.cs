e5a957757861258f7d8bc9c7c65c6dec0fb4d6c7
Owen <omockler@gmail.com>
2012-03-26 13:51:24 -0400

    Unbreaking the build

--- LunchVote/LunchVote/Filters/DayCreatedFilterAttribute.cs
+++ LunchVote/LunchVote/Filters/DayCreatedFilterAttribute.cs
@@ -1,11 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web.Mvc;
+using LunchVote.Models;
 
 namespace LunchVote.Filters
 {
     public class DayCreatedFilterAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            using (var db = new LunchVoteContext())
+            {
+                if(!db.Days.Where(d => d.Date == DateTime.Today).Any())
+                {
+                    db.Days.Add(new Day {Date = DateTime.Today, Id = Guid.NewGuid()});
+                    db.SaveChanges();
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
     }
 }
